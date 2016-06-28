d5c432552c1f6d084a58fdead5a2cebfd37c88ce
lambertbrady <bradylambert@wanderbydesign.co>
2016-06-27 19:06:44 -0500

    Updated Title Tag
    Changed Title tag to "Wander By Design"

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>Blog Template for Bootstrap</title>
+    <title>Wander By Design</title>
 
     <!-- Bootstrap core CSS -->
     <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet">
