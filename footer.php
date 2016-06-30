bd58490a7864ccd312d99ffccc28a703f13a5daa
flynn2812 <michaeldbianchi@gmail.com>
2016-06-29 21:39:36 -0500

    fixing problems created by my brackets port...

--- footer.php
+++ footer.php
@@ -5,7 +5,6 @@
       <p>
         <a href="#">Back to top</a>
       </p>
-    <?php wp_footer(); ?> 
     </footer>
 
 
@@ -14,9 +13,6 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-    
-  </body>
-</html>.min.js"></script>
     <?php wp_footer(); ?> 
   </body>
 </html>
