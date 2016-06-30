c8bae779d5446f7ad21ae9019e835c377b6b4d62
flynn2812 <michaeldbianchi@gmail.com>
2016-06-29 21:43:37 -0500

    fixing problems created by my brackets port...

--- front-page.php
+++ front-page.php
@@ -1,10 +1,6 @@
 <?php get_header(); ?>
 
 <!-- /.title stuff -->
-<h1>Header 1</h1>
-<p>Hey! This is a sample title page. Dope stuff yo.</p>
-
-<!--
 <div id="googlemap"></div>
 
 <h1 class="front-page-header">Header 1</h1>
@@ -19,5 +15,5 @@
     <p>My current thought is that we'll need javascript or something to change which content is visible</p>
     <p>It kinda depends on whether we want content to display on hovering over a country or on a click</p>
 </div>
--->
+
 <?php get_footer(); ?>
