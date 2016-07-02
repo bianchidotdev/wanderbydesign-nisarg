a74690d5c7d288a9bc2d6dce2cd96fc6013e33db
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-01 22:25:21 -0500

    change included Style sheets
    No longer include blog.css - instead use style.css
    
    Added: <link href="style.css" rel="stylesheet">
    Removed (commented out for now): <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">

--- header.php
+++ header.php
@@ -15,9 +15,9 @@
 
     <!-- Custom styles for this template -->
     
-    <!-- <link href="blog.css" rel="stylesheet"> -->
+    <link href="style.css" rel="stylesheet">
     
-    <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet">
+    <!-- <link href="<?php bloginfo('template_directory');?>/blog.css" rel="stylesheet"> -->
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
