e174b17a2c91ddc70962eb8d10a35da1d57796ff
flynn2812 <michaeldbianchi@gmail.com>
2016-07-04 10:46:45 -0500

    Minor changes

--- mfdb-test.php
+++ mfdb-test.php
@@ -1,13 +1,65 @@
-<?php
-/*
-Template Name: mfdb-test
-*/
-?>
-<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-<!-- Custom styles for title page-->
-<!-- <link href="<?php bloginfo('template_directory');?>/front-page.css" rel="stylesheet"> -->
+    <title>Wander By Design</title>
 
+    <!-- Bootstrap core CSS -->
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    
+    <!-- <link href="style.css" rel="stylesheet"> -->
+    
+      <link href="assets/css/style.css" rel="stylesheet">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+
+<!-- PREVIOUS HEADER MARKUP
+    <div class="blog-masthead">
+      <div class="container">
+        <nav class="blog-nav">
+          <a class="blog-nav-item active" href="#">Home</a>
+          <a class="blog-nav-item" href="#">About Us</a>
+          <a class="blog-nav-item" href="#">Blog</a>
+          <a class="blog-nav-item" href="#">Gallery</a>
+        </nav>
+      </div>
+    </div>
+ -->
+    
+    <div class="masthead">
+        <div class="logo">
+            <div>
+                <!--<a href="/Home"> LOGO PLACEHOLDER (WANDER BY DESIGN)</a>-->
+            </div>
+        </div>
+        <div class="nav-flex">
+          <a class="active" href="#">Home</a>
+          <a href="#">About Us</a>
+          <a href="#">Blog</a>
+          <a href="#">Gallery</a>
+        </div>
+    </div>
+
+    <div class="container">
+       <div class="blog-header">
+	       <h1 class="blog-title"><a href="#"></a>Wander by design</h1>
+      </div>
+      </div>
 
 <!-- Custom styles for title page-->
 <!-- <link href="assets/css/front-page.css" rel="stylesheet"> -->
@@ -77,11 +129,5 @@ google.maps.event.addDomListener(window, 'load', showGoogleMaps);
 
     </footer>
 
-
-    <!-- Bootstrap core JavaScript
-    ================================================== -->
-    <!-- Placed at the end of the document so the pages load faster -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   </body>
 </html>
