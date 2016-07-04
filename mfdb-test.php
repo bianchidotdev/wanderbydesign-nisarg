8541c4bbb73cf377f97e31465ba83a6359598513
flynn2812 <michaeldbianchi@gmail.com>
2016-07-04 10:43:52 -0500

    hard-coding footer to include javascript
    Just a test

--- mfdb-test.php
+++ mfdb-test.php
@@ -9,14 +9,19 @@ Template Name: mfdb-test
 <!-- <link href="<?php bloginfo('template_directory');?>/front-page.css" rel="stylesheet"> -->
 
 
+<!-- Custom styles for title page-->
+<!-- <link href="assets/css/front-page.css" rel="stylesheet"> -->
+
+
 <!-- /.title stuff -->
-<div id="googlemap"></div>
+
+<div id="google-map"></div>
 
 <div class="container">
     <h1 class="front-page-header">Header 1</h1>
 
     <p>Hey! This is a sample title page. Dope stuff yo.</p>
-</div>
+</div><!-- /.container -->
 
 <div class="front-page-sidebar">
     <h3>Sidebar header</h3>
@@ -27,4 +32,56 @@ Template Name: mfdb-test
     <p>It kinda depends on whether we want content to display on hovering over a country or on a click</p>
 </div>
 
-<?php get_footer(); ?>
+
+    <footer class="blog-footer">
+      <p>This is the personal travel blog of two wanderers.</p>
+      <p>
+        <a href="#">Back to top</a>
+      </p>
+      
+      
+      <!-- Include the Google Maps API library - required for embedding maps -->
+<script src="http://maps.googleapis.com/maps/api/js?key=AIzaSyB1InpKO3BR4G4B8ddipM4tHMwNy68rkUU"></script>
+ 
+<script type="text/javascript">
+ 
+// The latitude and longitude of your negocio / lugar
+var position = [27.1959739, 78.02423269999997];
+ 
+function showGoogleMaps() {
+ 
+    var latLng = new google.maps.LatLng(position[0], position[1]);
+ 
+    var mapOptions = {
+        zoom: 16, // initialize zoom nivel - the max valor is 21
+        streetViewControl: false, // hide the yellow Calle Vista pegman
+        scaleControl: true, // allow users to zoom the Google Map
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: latLng
+    };
+ 
+    map = new google.maps.Map(document.getElementById('google-map'),
+        mapOptions);
+ 
+    // Show the default red marker at the location
+    marker = new google.maps.Marker({
+        position: latLng,
+        map: map,
+        draggable: false,
+        animation: google.maps.Animation.DROP
+    });
+}
+ 
+google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+</script>
+
+    </footer>
+
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  </body>
+</html>
