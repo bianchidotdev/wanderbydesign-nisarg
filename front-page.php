9ec4eb0d5786ac2f535fabd7dde0e6178220209b
lambertbrady <bradylambert@Brady-Lamberts-MacBook-Pro.local>
2016-07-06 00:00:49 -0500

    Revert "fixing problems created by my brackets port..."
    This reverts commit a0c4ab704944da72eba565a189caeb97a45522cc.

--- front-page.php
+++ front-page.php
@@ -1,19 +1,7 @@
 <?php get_header(); ?>
 
 <!-- /.title stuff -->
-<div id="googlemap"></div>
-
-<h1 class="front-page-header">Header 1</h1>
-
+<h1>Header 1</h1>
 <p>Hey! This is a sample title page. Dope stuff yo.</p>
-
-<div class="front-page-sidebar">
-    <h3>Sidebar header</h3>
-    <p>This is content for the sidebar</p>
-    <p>We need to figure out how to best dynamically populate this</p>
-    <br>
-    <p>My current thought is that we'll need javascript or something to change which content is visible</p>
-    <p>It kinda depends on whether we want content to display on hovering over a country or on a click</p>
-</div>
 
 <?php get_footer(); ?>
