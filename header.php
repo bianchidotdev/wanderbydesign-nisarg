5d47bd1a69eab44d92311a93c877df762d71bf7e
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-05 22:11:24 -0500

    include Global.css

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
     
     <!-- <link href="style.css" rel="stylesheet"> -->
     
-    <link href="<?php bloginfo('template_directory');?>/style.css" rel="stylesheet">
+    <link href="<?php bloginfo('template_directory');?>/Global.css" rel="stylesheet">
 
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
