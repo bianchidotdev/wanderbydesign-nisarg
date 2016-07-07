f5b76e55582e76167467edef6787d82f4366ad82
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 20:47:41 -0500

    Create front-page.php

---
+++ front-page.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Custom front-page.php template
+ *
+ * Used to display the homepage of your
+ * WordPress site.
+ *
+ * @link http://themes.required.ch/?p=606
+ */
+
+get_header(); ?>
+
+<p> FRONT PAGE TEST </p>
