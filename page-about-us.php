8e51e026400ba6c91f8a3ffc74a06003519e75c8
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 22:07:14 -0500

    Update page-about-us.php
    Added the "entry-content" class - copied over from content.php

--- page-about-us.php
+++ page-about-us.php
@@ -10,8 +10,21 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+				<div class="entry-content">
+					<?php
+						the_content( sprintf(
+							/* translators: %s: Name of current post. */
+							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wander' ), array( 'span' => array( 'class' => array() ) ) ),
+							the_title( '<span class="screen-reader-text">"', '"</span>', false )
+						) );
+						wp_link_pages( array(
+							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wander' ),
+							'after'  => '</div>',
+						) );
+					?>
+				</div><!-- .entry-content -->
 
-    <p> custom page TEST (page-about-us.php) </p>
+    				<p> custom page TEST (page-about-us.php) </p>
     
 		</main><!-- #main -->
 	</div><!-- #primary -->
