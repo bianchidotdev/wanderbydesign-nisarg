47d85dbb175e1549cdf4b47a098f4b52d737174c
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 23:22:31 -0500

    Update page-about-us.php
    just add all of content.php into the file to try to make it work

--- page-about-us.php
+++ page-about-us.php
@@ -10,25 +10,42 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-			
-			<!-- from content.php -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<div class="entry-content">
-					<?php
-						the_content( sprintf(
-							/* translators: %s: Name of current post. */
-							wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wander' ), array( 'span' => array( 'class' => array() ) ) ),
-							the_title( '<span class="screen-reader-text">"', '"</span>', false )
-						) );
-						wp_link_pages( array(
-							'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wander' ),
-							'after'  => '</div>',
-						) );
-					?>
-				</div><!-- .entry-content -->
-			</article><!-- #post-## -->
 
-    			<p> custom page TEST (page-about-us.php) </p>
+<!-- from content.php -->
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php wander_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<?php
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wander' ), array( 'span' => array( 'class' => array() ) ) ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wander' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php wander_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
     
 		</main><!-- #main -->
 	</div><!-- #primary -->
