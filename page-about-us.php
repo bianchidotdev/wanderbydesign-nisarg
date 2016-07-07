0d5176b5cd3bfce668c6fca96997e23472668429
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 23:39:09 -0500

    Update page-about-us.php
    Reset - now the same as page.php

--- page-about-us.php
+++ page-about-us.php
@@ -11,37 +11,20 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
-<!-- from content.php -->
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-			if ( is_single() ) {
-				the_title( '<h1 class="entry-title">', '</h1>' );
-			} else {
-				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-			}
-		if ( 'post' === get_post_type() ) : ?>
-		<div class="entry-meta">
-			<?php wander_posted_on(); ?>
-		</div><!-- .entry-meta -->
-		<?php
-		endif; ?>
-	</header><!-- .entry-header -->
+			<?php
+			while ( have_posts() ) : the_post();
+				get_template_part( 'template-parts/content', 'page' );
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
+			endwhile; // End of the loop.
+			?>
 
-	<div class="entry-content">
-		<?php
-			the_content();
-		?>
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-		<?php wander_entry_footer(); ?>
-	</footer><!-- .entry-footer -->
-</article><!-- #post-## -->
-    
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
 <?php
 get_sidebar();
 get_footer();
+?>
