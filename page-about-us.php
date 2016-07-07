8c8250b9a19c0f0e653ff9998e62a916104c7d65
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 21:22:58 -0500

    Create page-about-us.php
    Copied from page.php, with the Loop removed (the one that retrieves blog posts). I also added the following for testing: <p> custom page TEST (page-about-us.php) </p>

---
+++ page-about-us.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * The template for displaying the ----About Us---- page. The slug is "about-us"
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package wander
+ */
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+    <p> custom page TEST (page-about-us.php) </p>
+    
+		</main><!-- #main -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar();
+get_footer();
