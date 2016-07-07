e21f15e3c3a80d27978450004e660638f2830598
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-06 23:34:33 -0500

    Update page-about-us.php
    remove the_content() function arguments

--- page-about-us.php
+++ page-about-us.php
@@ -30,15 +30,7 @@ get_header(); ?>
 
 	<div class="entry-content">
 		<?php
-			the_content( sprintf(
-				/* translators: %s: Name of current post. */
-				wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'wander' ), array( 'span' => array( 'class' => array() ) ) ),
-				the_title( '<span class="screen-reader-text">"', '"</span>', false )
-			) );
-			wp_link_pages( array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wander' ),
-				'after'  => '</div>',
-			) );
+			the_content();
 		?>
 	</div><!-- .entry-content -->
 
