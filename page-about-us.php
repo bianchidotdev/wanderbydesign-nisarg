1675f28833598f2cba28762cd3c7508445c33743
flynn2812 <michaeldbianchi@gmail.com>
2016-07-16 03:11:13 +0000

    about-us updates

--- page-about-us.php
+++ page-about-us.php
@@ -14,27 +14,26 @@ get_header(); ?>
 
 		<div class="container">
             <div class="row">
-				<div id="primary" class="col-md-9 content-area">
-					<main id="main" class="site-main" role="main">
-
-						<?php while ( have_posts() ) : the_post(); ?>
-
-							<?php get_template_part( 'template-parts/content', 'page-about-us' ); ?>
-
-							<?php
-								// If comments are open or we have at least one comment, load up the comment template.
-								if ( comments_open() || get_comments_number() ) :
-									comments_template();
-								endif;
-							?>
-
-						<?php endwhile; // End of the loop. ?>
-
-					</main><!-- #main -->
-				</div><!-- #primary -->
-
-				<?php get_sidebar('sidebar-1'); ?>		
-
+            	<div id="about-us">
+					<div id="primary" class="col-md-9 content-area">
+						<main id="main" class="site-main" role="main">
+	
+							<?php while ( have_posts() ) : the_post(); ?>
+	
+								<?php get_template_part( 'template-parts/content', 'page-about-us' ); ?>
+	
+								<?php
+									// If comments are open or we have at least one comment, load up the comment template.
+									if ( comments_open() || get_comments_number() ) :
+										comments_template();
+									endif;
+								?>
+	
+							<?php endwhile; // End of the loop. ?>
+	
+						</main><!-- #main -->
+					</div><!-- #primary -->
+				</div><!-- #about-us -->
 			</div> <!--.row-->            
         </div><!--.container-->
         <?php get_footer(); ?>
