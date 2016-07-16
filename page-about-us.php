13c0fe4bf33928f25e688c82500c57753d49d148
flynn2812 <michaeldbianchi@gmail.com>
2016-07-16 18:18:21 +0000

    move flex container into .main

--- page-about-us.php
+++ page-about-us.php
@@ -31,12 +31,12 @@ get_header(); ?>
 	
 							<?php endwhile; // End of the loop. ?>
 	
+							<div class="about-us-flex">
+						
+							</div><!-- .about-us-flex -->
+	
 						</main><!-- #main -->
 					</div><!-- #primary -->
-					
-					<div class="about-us-flex">
-						
-					</div><!-- .about-us-flex -->
 					
 				</div><!-- #about-us -->
 			</div> <!--.row-->            
