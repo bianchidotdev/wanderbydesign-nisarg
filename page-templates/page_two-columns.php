682d3709039276d847a214f62a82aa5fd3d2d51e
michael bianchi <michaeldbianchi@gmail.com>
2016-07-17 14:51:36 -0500

    made full width

--- page-templates/page_two-columns.php
+++ page-templates/page_two-columns.php
@@ -14,7 +14,7 @@ get_header(); ?>
 
 		<div class="container">
             <div class="row">
-				<div id="primary" class="col-md-9 content-area">
+				<div id="primary" class="col-md-12 content-area ">
 					<main id="main" class="site-main" role="main">
 
 						<?php while ( have_posts() ) : the_post(); ?>
