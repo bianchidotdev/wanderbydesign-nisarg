8fc47d73ec772d2aae3feb92f8f6fddb5c1732f3
lambertbrady <bradylambert@wanderbydesign.co>
2016-07-17 17:23:39 -0500

    Remove about-us id

--- page-about-us.php
+++ page-about-us.php
@@ -12,29 +12,27 @@
 
 get_header(); ?>
 
-		<div class="container">
-            <div class="row">
-            	<div id="about-us">
-					<div id="primary" class="col-md-12 content-area">
-						<main id="main" class="site-main" role="main">
-	
-							<?php while ( have_posts() ) : the_post(); ?>
-	
-								<?php get_template_part( 'template-parts/content', 'page-about-us' ); ?>
-	
-								<?php
-									// If comments are open or we have at least one comment, load up the comment template.
-									if ( comments_open() || get_comments_number() ) :
-										comments_template();
-									endif;
-								?>
-	
-							<?php endwhile; // End of the loop. ?>
-	
-						</main><!-- #main -->
-						
-					</div><!-- #primary -->
-				</div><!-- #about-us -->
-			</div> <!--.row-->            
+	<div class="container">
+            	<div class="row">
+            		<div id="primary" class="col-md-12 content-area">
+				<main id="main" class="site-main" role="main">
+
+					<?php while ( have_posts() ) : the_post(); ?>
+
+						<?php get_template_part( 'template-parts/content', 'page-about-us' ); ?>
+
+						<?php
+							// If comments are open or we have at least one comment, load up the comment template.
+							if ( comments_open() || get_comments_number() ) :
+								comments_template();
+							endif;
+						?>
+
+					<?php endwhile; // End of the loop. ?>
+
+				</main><!-- #main -->
+				
+			</div><!-- #primary -->
+		</div> <!--.row-->            
         </div><!--.container-->
         <?php get_footer(); ?>
