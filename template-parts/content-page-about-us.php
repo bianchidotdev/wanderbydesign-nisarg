4574ec952f6b4616b4bc9c158c20e389a37f7409
flynn2812 <michaeldbianchi@gmail.com>
2016-08-19 06:30:55 +0200

    Adding flickr link

--- template-parts/content-page-about-us.php
+++ template-parts/content-page-about-us.php
@@ -87,6 +87,7 @@
 					.ig-b-32 { background-image: url(//badges.instagram.com/static/images/ig-badge-sprite-32@2x.png); background-size: 60px 178px; } }</style>
 					Follow me here:
 					<a href="https://www.instagram.com/michaeldbianchi/?ref=badge" class="ig-b- ig-b-32"><img src="//badges.instagram.com/static/images/ig-badge-32.png" alt="Instagram" /></a>
+					<a href="http://www.flickr.com/photos/143059008@N02/" class="ig-b-" title="See my photos on Flickr!"><img src="https://s.yimg.com/pw/images/goodies/white-large-chiclet.png" width="44" height="44" alt=""></a>
 			</div>
 		</div><!-- .about-us-flex -->
 		
