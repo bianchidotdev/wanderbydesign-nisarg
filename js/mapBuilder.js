9ebc9423937490fed0874ee764d290f715a88bd6
lambertbrady <lambertbrady14@gmail.com>
2017-03-06 15:05:27 -0600

    Update mapBuilder.js

--- js/mapBuilder.js
+++ js/mapBuilder.js
@@ -1,4 +1,4 @@
-var countriesFileExt = 'wp-content/themes/nisarg/data/countriesSouthAmerica.geo.json'
+var countriesFileExt = 'wp-content/themes/nisarg/data/countriesSouthAmerica.geo.json';
 var pathsFileExt = 'wp-content/themes/nisarg/data/myMapsURLs.txt';
 var extArray = ["category", "gallery"];
 var map;
